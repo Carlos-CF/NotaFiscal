05e99bd621e7721e5a1babcdafb7221ed8174f0b
Carlos-CF <carloscaetanofernandes@hotmail.com>
2024-06-12 10:59:15 -0300

    Iniciando o Projeto

---
+++ src/test/java/com/br/NotaFiscal/NotaFiscalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.NotaFiscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaFiscalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
